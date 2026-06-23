388563c49273ae93940c531bc8df45d40f75ec62
Nikita Hertrampf <nikita.hertrampf@heptacom.de>
2026-06-23 18:03:22 +0200

    add deprecated fallbacks for moved MetadataClientContract and ModifiedRedirectBehaviourClientContract

---
+++ src/Contract/MetadataClientContract.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\AdminOpenAuth\Contract;
+
+/**
+ * @deprecated Use {@see \Heptacom\AdminOpenAuth\Contract\Client\MetadataClientContract} instead.
+ */
+interface MetadataClientContract extends \Heptacom\AdminOpenAuth\Contract\Client\MetadataClientContract
+{
+}
---
+++ src/Contract/ModifiedRedirectBehaviourClientContract.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\AdminOpenAuth\Contract;
+
+/**
+ * @deprecated Use {@see \Heptacom\AdminOpenAuth\Contract\Client\ModifiedRedirectBehaviourClientContract} instead.
+ */
+interface ModifiedRedirectBehaviourClientContract extends \Heptacom\AdminOpenAuth\Contract\Client\ModifiedRedirectBehaviourClientContract
+{
+}
